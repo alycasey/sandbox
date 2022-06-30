61857bbb3d780537a93ddd4a01b1a1c2e6a53ac2
Alex Merkert <alex.merkert@oliveai.com>
2022-06-30 08:24:26 -0400

    something else

--- extract-emoji.js
+++ extract-emoji.js
@@ -1,21 +1,25 @@
 Array.from(document.querySelector('[data-qa=slack_kit_list]').children).forEach(e => {
   const img = e.querySelector('img.p-customize_emoji_list__image');
-  const [_, filetype] = img.src.split('.');
+  const x = img.src.split('.');
+  const filetype = x.reverse()[0];
   const name = img.alt;
-  var c = document.createElement("canvas");
-  var ctx = c.getContext("2d");
-  c.width = img.naturalWidth;
-  c.height = img.naturalHeight;
-  ctx.drawImage(img, 0, 0);
-  c.toBlob(blob => {
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = url;
-    // the filename you want
-    a.download = `${name}.${filetype}`;
-    document.body.appendChild(a);
-    a.click();
-    window.URL.revokeObjectURL(url);
-  }, "image/jpeg", 0.75);
+  window.sessionStorage.setItem(`*EMOJI--${name}.${filetype}`, img.src);
 });
+
+
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', img.src);
+xhr.responseType = 'blob';
+xhr.onload = e => {
+  const url = window.URL.createObjectURL(this.response);
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  // the filename you want
+  a.download = `${name}.${filetype}`;
+  document.body.appendChild(a);
+  a.click();
+  window.URL.revokeObjectURL(url);
+};
+xhr.send(); 
